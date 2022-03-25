f8510ac23894f85e1ebde177f2931a9735d5ee29
unknown <michal.o.struck@gmail.com>
2022-03-25 22:18:49 +0100

    styling fixed and cleaned up

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -20,36 +20,30 @@ export class Carousel extends Component {
     const { images } = this.props;
     return (
       <div
-        className="flex
-      justify-center items-center "
-      >
-        <div
-          className="flex 
+        className="flex 
       items-center 
-      h-96 mt-2 
-      "
-        >
-          <img
-            src={images[active]}
-            className="max-w-[45%] max-h-96 rounded-md mt-8"
-            alt="animal"
-          />
-          <div className="w-6/12">
-            {images.map((photo, index) => (
-              <img
-                alt="img"
-                data-index={index}
-                onClick={this.handleIndexClick}
-                key={photo}
-                src={photo}
-                className={
-                  index === active
-                    ? "w-28 h-28 rounded-full inline-block m-4 cursor-pointer border-2 border-solid border-black"
-                    : "w-28 h-28 rounded-full inline-block m-4 cursor-pointer border-solid border-black opacity-60"
-                }
-              />
-            ))}
-          </div>
+      h-96"
+      >
+        <img
+          src={images[active]}
+          className="max-w-[45%] max-h-96 rounded-md  "
+          alt="animal"
+        />
+        <div className="w-6/12">
+          {images.map((photo, index) => (
+            <img
+              alt="img"
+              data-index={index}
+              onClick={this.handleIndexClick}
+              key={photo}
+              src={photo}
+              className={
+                index === active
+                  ? "float-left w-28 h-28 rounded-full inline-block m-4 cursor-pointer border-2 border-solid border-black"
+                  : "float-left w-28 h-28 rounded-full inline-block m-4 cursor-pointer border-solid border-black opacity-60"
+              }
+            />
+          ))}
         </div>
       </div>
     );
