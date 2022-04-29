02247ba8f4886a30dfb12078d70827d107989860
unknown <michal.o.struck@gmail.com>
2022-04-29 22:28:52 +0200

    fix: menu button spamming is no longer possible

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import Expandable, { ExpandableContext } from "../common/Expandable/Menu";
+import { CSSTransition } from "react-transition-group";
+import "../common/index.css";
 
 const Navbar = () => {
   const [turnState, setTurnState] = useState(false);
+  const [inProp, setInProp] = useState(false);
+
   return (
     <div className="fixed w-full h-1/6 top-10 right-5">
       <div className="h-1/6 grid grid-cols-1 grid-rows-2 place-items-end">
@@ -14,44 +18,42 @@ const Navbar = () => {
           <ExpandableContext.Consumer>
             {({ expanded, toggleExpand }) => (
               <Expandable.Icon style="">
-                {expanded ? (
+                {
                   <button
-                    onClick={() => {
-                      setTurnState(true);
-                      toggleExpand();
-                    }}
-                    onAnimationEnd={() => setTurnState(false)}
-                    className={`${
-                      turnState && "animate-turn360"
-                    } text-7xl float-right pr-1 z-10`}
+                    onClick={
+                      turnState
+                        ? () => {}
+                        : () => {
+                            setTurnState(true);
+                            toggleExpand();
+                            setInProp(expanded ? false : true);
+                          }
+                    }
+                    onAnimationEnd={() =>
+                      setTimeout(() => setTurnState(false), 10)
+                    }
+                    className={`${turnState && "animate-turn360"}
+                        text-7xl float-right pr-1 z-10`}
                   >
-                    ‒ {/* it's the figure dash */}
+                    {expanded ? "‒" /* it's the figure dash */ : "+"}
                   </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      setTurnState(true);
-                      toggleExpand();
-                    }}
-                    onAnimationEnd={() => setTurnState(false)}
-                    className={`${
-                      turnState && "animate-turn360"
-                    } text-7xl float-right z-10  `}
-                  >
-                    +
-                  </button>
-                )}
+                }
               </Expandable.Icon>
             )}
           </ExpandableContext.Consumer>
-          <Expandable.Body style="bg-red-200 opacity-70 text-right mt-auto p-2">
-            <p>
-              Want to adopt a pet? Try on{" "}
-              <a className="underline" href="https://www.petfinder.com/">
-                petfinder
-              </a>
-            </p>
-          </Expandable.Body>
+          <CSSTransition in={inProp} timeout={1} classNames="bodyTransition">
+            <Expandable.Body
+              key="body"
+              style="bg-red-200 opacity-70 text-right mt-auto "
+            >
+              <p className="p-2">
+                Want to adopt a pet? Try on{" "}
+                <a className="underline" href="https://www.petfinder.com/">
+                  petfinder
+                </a>
+              </p>
+            </Expandable.Body>
+          </CSSTransition>
         </Expandable>
       </div>
     </div>
