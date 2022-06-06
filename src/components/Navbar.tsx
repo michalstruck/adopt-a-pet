346e742cb1ac090b04ebaff183c94d05e624958b
unknown <michal.o.struck@gmail.com>
2022-06-06 17:41:29 +0200

    fix: fixed the exp. menu icon animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import Expandable, { ExpandableContext } from "../common/Expandable/Menu";
-import { CSSTransition } from "react-transition-group";
 import { Link } from "react-router-dom";
 import { useRandomPetId } from "../common/small_hooks";
+import { useSpring, animated } from "@react-spring/web";
 import "../common/index.css";
 
 const Navbar = () => {
-  const [state, setState] = useState({ turnState: false, inProp: false });
+  const [turnState, setTurnState] = useState(false);
   const { randomize, randomPetId } = useRandomPetId();
+  const springProps = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+
   return (
     <div className="fixed w-auto h-0 top-14 right-5">
       <div className="h-1/6 grid grid-cols-1 grid-rows-2 place-items-end">
@@ -22,21 +24,18 @@ const Navbar = () => {
                 {
                   <button
                     onClick={
-                      state.turnState
+                      turnState
                         ? () => {}
                         : () => {
-                            setState({ turnState: false, inProp: !expanded });
+                            setTurnState(true);
                             toggleExpand();
                           }
                     }
                     onAnimationEnd={() =>
-                      setTimeout(
-                        () => setState({ ...state, turnState: false }),
-                        10
-                      )
+                      setTimeout(() => setTurnState(false), 10)
                     }
-                    className={`${state.turnState && "animate-turn360"}
-                        text-7xl float-right pr-1 z-10`}
+                    className={`${turnState ? "animate-turn360" : ""}
+                text-7xl float-right pr-1 z-10`}
                   >
                     {expanded ? "‒" /* it's the figure dash */ : "+"}
                   </button>
@@ -44,44 +43,36 @@ const Navbar = () => {
               </Expandable.Icon>
             )}
           </ExpandableContext.Consumer>
-          <CSSTransition
-            in={state.inProp}
-            timeout={4000}
-            classNames="bodyTransition"
+          <Expandable.Body
+            key="body"
+            style="bg-red-200 opacity-70 text-right mt-auto "
           >
-            <Expandable.Body
-              key="body"
-              style="bg-red-200 opacity-70 text-right mt-auto "
-            >
-              <div className="p-2 text-left">
-                <h1 className="text-4xl float-left font-semibold">
-                  Quick start
-                </h1>
-                <br />
-                <section className="float-left">
-                  <div className="text-3xl">
-                    Feeling lucky? Try a{" "}
-                    <button onClick={randomize} className="w-fit">
-                      {
-                        <Link
-                          to={`/details/${randomPetId}`}
-                          className="w-100% flex mt-7 mb-8 underline decoration-2"
-                        >
-                          random pet.
-                        </Link>
-                      }
-                    </button>
-                  </div>
-                  <footer className="text-lg">
-                    Want to adopt a pet? Try on{" "}
-                    <a className="underline" href="https://www.petfinder.com/">
-                      petfinder
-                    </a>
-                  </footer>
-                </section>
-              </div>
-            </Expandable.Body>
-          </CSSTransition>
+            <div className="p-2 text-left">
+              <h1 className="text-4xl float-left font-semibold">Quick start</h1>
+              <br />
+              <section className="float-left">
+                <div className="text-3xl">
+                  Feeling lucky? Try a{" "}
+                  <button onClick={randomize} className="w-fit">
+                    {
+                      <Link
+                        to={`/details/${randomPetId}`}
+                        className="w-100% flex mt-7 mb-8 underline decoration-2"
+                      >
+                        random pet.
+                      </Link>
+                    }
+                  </button>
+                </div>
+                <footer className="text-lg">
+                  Want to adopt a pet? Try on{" "}
+                  <a className="underline" href="https://www.petfinder.com/">
+                    petfinder
+                  </a>
+                </footer>
+              </section>
+            </div>
+          </Expandable.Body>
         </Expandable>
       </div>
     </div>
