81dd9b0c1dd6d87bf7c461de5e2468ff5ff3931d
unknown <michal.o.struck@gmail.com>
2022-06-15 15:50:35 +0200

    Updates

--- src/common/ThemeContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const ThemeContext = createContext<[string, (theme: string) => void]>([
-  "rgb(153 27 27)",
-  () => {},
-]);
-
-export default ThemeContext;
