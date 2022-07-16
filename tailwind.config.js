448ea447bac3e2fe2b30108b849ff92979784103
unknown <michal.o.struck@gmail.com>
2022-07-16 23:31:22 +0200

    style: tailwind elevation plugin added, buttons restyled

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwindcss-elevation")(["responsive"]),
+  ],
 };
