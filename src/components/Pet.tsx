40e584a4fc592e9fef1faea0cd78757e3a380776
unknown <michal.o.struck@gmail.com>
2023-01-14 00:27:23 +0100

    style: Pet component refactored + styles for smaller screens updated

--- src/components/Pet.tsx
+++ src/components/Pet.tsx
@@ -1,32 +1,37 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { Pet as PetType } from "../common/types/APIResponsesTypes";
+import Divider from "./Divider";
 
 const Pet = ({ name, animal, breed, images = [], location, id }: PetType) => {
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+
   if (images.length) {
     hero = images[0]!;
   }
 
   return (
     <>
-      <Link to={`/details/${id}`} className=" flex flex-row mt-7 pb-8">
+      <Link
+        to={`/details/${id}`}
+        className="flex flex-col items-center my-4 sm:items-start sm:flex-row sm:mx-8"
+      >
         <img
           data-testid="thumbnail"
           src={hero}
           alt={name}
-          className="w-48 h-48 rounded-full shadow-md shadow-stone-500"
+          className="w-36 h-36 sm:w-48 sm:h-48 rounded-full shadow-md shadow-stone-500"
         />
-        <div className="ml-4 flex flex-col">
-          <h1 className="text-3xl font-bold flex flex-shrink flex-wrap break-all">
+        <div className="flex flex-col items-center sm:items-start sm:ml-4">
+          <h1 className="flex flex-shrink flex-wrap text-3xl font-bold break-all mt-1">
             {name}
           </h1>
-          <h1 className="text-lg flex flex-shrink">
+          <h1 className="flex flex-shrink text-lg">
             {animal} - {breed} - {location}
           </h1>
         </div>
       </Link>
-      <div className=" border-b-black border-b-2" />
+      <Divider />
     </>
   );
 };
