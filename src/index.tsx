852627947a65c6a34f94236f941d9982f3e3976e
unknown <michal.o.struck@gmail.com>
2023-01-14 01:41:34 +0100

    fix: vercel build fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./common/index.css";
+import "./index.css";
 import App from "./App";
 
 const el = document.getElementById("root");
