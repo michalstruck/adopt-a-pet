bfcba6844f5775fb2652ff2159347e809b9037de
unknown <michal.o.struck@gmail.com>
2023-01-16 19:32:05 +0100

    refactor+fix tests

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 /**
  * Simlpe Divider component, wrap in a div to add styles
  *
--- src/components/Results.placeholder..tsx
+++ src/components/Results.placeholder..tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import Divider from "./Divider";
 import Skeleton from "./Skeleton";
 
